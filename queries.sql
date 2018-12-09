0904d7ad80afef15016996a5ea5cdec5a89fd734
Sasha <alxblag@ya.ru>
2018-12-09 15:38:28 +0200

    БД-2

--- queries.sql
+++ queries.sql
@@ -1,2 +1,21 @@
+-- Добавляем список категорий;
+
+INSERT INTO `categories`
+       (name)
+VALUES
+       ('Доски и лыжи'),
+       ('Крепления'),
+       ('Ботинки'),
+       ('Одежда'),
+       ('Инструменты'),
+       ('Разное');
+
+-- Добавляем пользователей
+
+INSERT INTO `users`
+       (email, password, name, contact, avatar)
+VALUES
+       ('alexblag@gmail.com', 'qwerty123', 'Alex', '022-520330', '/img/ava.png'),
+       ('alexblag@ya.ru', 'qweqwe123', 'Masha', '079123456', '/img/ava1.png');
 
 
