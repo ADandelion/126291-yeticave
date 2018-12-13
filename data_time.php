7d801ae830e5a46d2df19c100d454ed8765eb259
Sasha <alxblag@ya.ru>
2018-12-13 20:57:40 +0200

    Запросы

--- data_time.php
+++ data_time.php
@@ -5,5 +5,3 @@ $currentTime = date_create(date("Y-m-d H:i"));
 $midnightTime = date_create(date("Y-m-d H:i", strtotime("tomorrow")));
 $interval = date_diff($midnightTime, $currentTime);
 $endDay = $interval->format('%H:%I');
-
-var_dump($currentTime);
