01d3ccb86cda329ab58ac0d55500548425f17582
Sasha <alxblag@ya.ru>
2018-12-15 21:06:41 +0200

    Добавляем данные из БД на фронт

--- data_time.php
+++
@@ -1,6 +0,0 @@
-<?php
-date_default_timezone_set("Europe/Chisinau");
-
-//$date1 = date_create('$date');
-//$lot_date = $date1->format("Y-m-d H:i");
-//$endDay = $interval->format('%H:%I');
--- init.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'functions.php';
-$db_auth = require_once 'config/db.php';
-$link = mysqli_connect($db_auth['host'], $db_auth['user'], $db_auth['password'], $db_auth['database']);
-mysqli_set_charset($link, "utf8");
